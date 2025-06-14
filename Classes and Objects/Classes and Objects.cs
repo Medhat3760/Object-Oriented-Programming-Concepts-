76d3316f09a10c5cf564b6cb5c0b9525b4ec3d1f
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-06-14 20:20:59 +0300

    Add files via upload

---
+++ Classes and Objects/Classes and Objects.cs
@@ -0,0 +1,38 @@
+﻿namespace Classes_and_Objects
+{
+    internal class Program
+    {
+
+        class clsPerson
+        {
+            //this private by default and will not be accessed from outside the class
+            //for internal use only
+            int x;
+
+            public string firstName;
+            public string lastName;
+
+            public string FullName()
+            {
+
+                return firstName + " " + lastName;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            clsPerson person1 = new clsPerson();
+
+            person1.firstName = "Abdelrahman";
+            person1.lastName = "Medhat";
+
+            Console.WriteLine(person1.FullName());
+
+            string s1;
+
+        }
+    }
+}
