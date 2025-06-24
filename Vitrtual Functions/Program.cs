f594b87b2aa140dedd69b0c572756fdae1f72848
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-06-24 08:22:06 +0300

    Virtual Functions Demo

---
+++ Vitrtual Functions/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Vitrtual_Functions
+{
+    internal class Program
+    {
+
+        class clsPerson
+        {
+
+            public virtual void Print()
+            {
+                Console.WriteLine("Hi, i'm Person.");
+            }
+
+        }
+
+        class clsEmployee : clsPerson
+        {
+
+            public override void Print()
+            {
+                Console.WriteLine("Hi, i'm Employee.");
+            }
+
+        }
+
+        class clsStudent : clsPerson
+        {
+
+            public override void Print()
+            {
+                Console.WriteLine("Hi, i'm Student.");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            clsEmployee employee1 = new clsEmployee();
+            employee1.Print();
+
+            clsStudent student1 = new clsStudent();
+            student1.Print();
+
+            Console.WriteLine();
+
+            // Upcasting to base class reference
+            clsPerson person1 = employee1;
+            person1.Print();
+
+            clsPerson person2 = student1;
+            person2.Print();
+
+        }
+    }
+}
