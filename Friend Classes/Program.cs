f2ba95e067660a6ec60dfdd7711df64f04e348b5
AbdelRahman Medhat <72521364+Medhat3760@users.noreply.github.com>
2025-06-28 05:42:43 +0300

    Friend Class Demo

---
+++ Friend Classes/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Friend_Classes
+{
+
+    // Note:
+    // In C#, there is no direct equivalent to C++'s friend keyword,
+    // because C# enforces stricter access control
+    // and does not allow one class to access private members of another
+    // unless they're nested or use other mechanisms (Changing the private modifier to internal modifier).
+
+    internal class Program
+    {
+
+        class clsA
+        {
+
+            internal int _var1;
+
+            public int var2;
+
+            public clsA()
+            {
+                _var1 = 10;
+                var2 = 20;
+            }           
+
+        }
+
+        class clsB
+        {
+
+            public void Display(clsA a)
+            {
+
+                Console.WriteLine("\nThe value of Var1 = " + a._var1);
+                Console.WriteLine("\nThe value of Var2 = " + a.var2);
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            clsA a = new clsA();
+            clsB b = new clsB();
+
+            Console.WriteLine(a._var1);
+
+            b.Display(a);
+
+            Console.ReadKey();
+
+        }
+    }
+}
