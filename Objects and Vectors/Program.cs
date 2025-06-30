0fc4332a9adebd6809aab666fb5e56566138be8c
Medhat3760 <Medhat3760@gmail.com>
2025-06-30 13:09:53 +0200

    Insert Temporary Objects to a List

---
+++ Objects and Vectors/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Objects_and_Vectors
+{
+
+    class clsA
+    {
+
+        public int x;
+
+        public clsA(int val)
+        {
+            x = val;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\nThe value of x = "+x);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<clsA> list1 = new List<clsA>();
+
+            short numberOfObjects = 5;
+
+            for(short i = 0; i < numberOfObjects; i++)
+            {
+                list1.Add(new clsA(i));
+            }
+
+            for(short i = 0; i < numberOfObjects; i++)
+            {
+                list1[i].Print();
+            }
+
+        }
+    }
+}
